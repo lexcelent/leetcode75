baf4c08c2b43311ff3d2798535a5e6eb4915fead
Aleksey <lexanorilsk@gmail.com>
2025-07-02 13:24:01 +0300

    add 238 and 605

--- array-strings/main.go
+++ array-strings/main.go
@@ -45,6 +45,87 @@ func isPalindrome(s string) bool {
 	return true
 }
 
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	// 605. Can Place Flowers
+	// На будущее: переписать код под switch-case
+
+	if n == 0 {
+		// We can place zero flowers
+		return true
+	}
+
+	placeFlower := func(i int) {
+		flowerbed[i] = 1
+		n--
+	}
+
+	if len(flowerbed) == 1 {
+		// Process only one element
+		if flowerbed[0] == 0 {
+			placeFlower(0)
+		}
+		return n == 0
+	}
+
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 1 {
+			continue
+		}
+
+		if i == 0 {
+			// Правило для левой границы
+			if flowerbed[i+1] == 0 {
+				placeFlower(i)
+			}
+			continue
+		}
+
+		if i == len(flowerbed)-1 {
+			// Правило для правой границы
+			if flowerbed[i-1] == 0 {
+				placeFlower(i)
+			}
+			continue
+		}
+
+		if flowerbed[i-1] == 0 && flowerbed[i+1] == 0 {
+			placeFlower(i)
+		}
+
+		if n <= 0 {
+			return true
+		}
+	}
+
+	return n == 0
+}
+
+func productExceptSelf(nums []int) []int {
+	// 238. Product of Array Except Self
+	// Нужно будет повторить
+
+	res := make([]int, len(nums))
+
+	for i := range res {
+		res[i] = 1
+	}
+
+	var prefix, postfix = 1, 1
+	for i := 0; i < len(nums); i++ {
+		res[i] *= prefix
+		prefix *= nums[i]
+		fmt.Printf("i: %d\nres: %v\nprefix: %d\n\n", i, res, prefix)
+	}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] *= postfix
+		postfix *= nums[i]
+	}
+
+	return res
+}
+
 func main() {
 	fmt.Println(isPalindrome(" "))
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4, 5}))
 }
